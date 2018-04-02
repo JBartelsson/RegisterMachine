5974d3aa78f929b897682ccb2c421a588560f7ba
DrDegu <JoBa2902@googlemail.com>
2018-04-02 02:06:22 +0200

    functioning log

--- ugraphicsform.pas
+++ ugraphicsform.pas
@@ -19,11 +19,11 @@ type
     Editor: TMemo;
     InfoLabel: TLabel;
     LoadFile: TButton;
-    Memo1: TMemo;
     OpenRegister: TOpenDialog;
     Pages: TPageControl;
     HeadingLeft: TPanel;
     RegisterSG: TStringGrid;
+    ExecuteSG: TStringGrid;
     TabSetUp: TTabSheet;
     TabWrite: TTabSheet;
     TabExecute: TTabSheet;
@@ -32,6 +32,7 @@ type
     procedure ExecuteBtnClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure LoadFileClick(Sender: TObject);
+    procedure initializeExecuteSG(registerAmount : Integer);
   private
     { private declarations }
   public
@@ -59,11 +60,31 @@ begin
 
 end;
 
+procedure TForm1.initializeExecuteSG(registerAmount: Integer);
+var
+  i : Integer;
+begin
+    with ExecuteSG do
+    begin
+      Cells[0, 0] := 'b';
+      Cells[1, 0] := 'Zeile';
+      Cells[2, 0] := 'Effekt';
+      ColCount:= 3 + registerAmount;
+      RowCount := Length(regM.GetExecuteLog) + 1;
+    end;
+    for i := 0 to registerAmount - 1 do
+    begin
+      ExecuteSG.Cells[i + 3, 0 ]:= 'c(' + IntToStr(i) + ')';
+    end;
+end;
+
 procedure TForm1.CloseBtnClick(Sender: TObject);
 begin
   Close;
 end;
 
+
+//CREATE
 procedure TForm1.CreateMachine(Sender: TObject);
 var
   i : Integer;
@@ -96,10 +117,13 @@ begin
 
 end;
 
+
+//EXECUTE
 procedure TForm1.ExecuteBtnClick(Sender: TObject);
 var
   values : array of Integer;
-  i : Integer;
+  i,j : Integer;
+  h : commandLine;
 begin
   SetLength(values, RegisterSG.ColCount - 1 );
   for i := 0 to RegisterSG.ColCount - 1 do
@@ -107,6 +131,28 @@ begin
     values[i] := StrtoInt(RegisterSG.Cells[i, 1]);
   end;
   regM.Execute(values);
+  TabExecute.TabVisible:= true;
+  Pages.ActivePage := TabExecute;
+
+  initializeExecuteSG(Length(regM.GetRegisterData));
+  for i := 1 to Length(regM.GetExecuteLog) do
+  begin
+    with ExecuteSG do
+    begin
+      Cells[0, i] := regM.GetExecuteLog[i - 1].b;
+      h := regM.GetExecuteLog[i - 1].command;
+      case h.command of
+           'END' : Cells[1, i] := h.command;
+           '' :   Cells[1, i] := '';
+           else Cells[1, i] := h.command + ' ' + IntToStr(h.value);
+      end;
+      Cells[2, i] := regM.GetExecuteLog[i - 1].sysOutput;
+    end;
+    for j := 0 to High(regM.GetExecuteLog[i - 1].registers) do
+    ExecuteSG.Cells[3 + j, i] := IntToStr(regM.GetExecuteLog[i -1].registers[j]);
+  end;
+
+
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
--- uregistermachine.pas
+++ uregistermachine.pas
@@ -18,13 +18,14 @@ type
   commandList = array of commandLine;
 
   commandLog = record
+    b : String;
     command: commandLine;
     sysOutput : String;
     registers : registerArray;
 end;
 
 type
-
+   commandLogSum = array of commandLog;
 
   { RegisterMachine }
 
@@ -34,10 +35,23 @@ type
     registerData: registerArray;
     programData: commandList;
     rawStringData: TStringList;
-    ExecuteLog: TStringList;
+    executeLog: commandLogSum;
     errorMessage: string;
     function LOAD(c : Integer) : String;
     function STORE(c : Integer) : String;
+    function ADD(c : Integer) : String;
+    function SUB(c : Integer) : String;
+    function MULT(c : Integer) : String;
+    function &DIV(c : Integer) : String;
+    function CLOAD(c : Integer) : String;
+    function CADD(c : Integer) : String;
+    function CSUB(c : Integer) : String;
+    function CMULT(c : Integer) : String;
+    function CDIV(c : Integer) : String;
+    function JZERO(var j, b : Integer) : String;
+    function JNZERO(var j, b : Integer) : String;
+    function &GOTO(var j, b : Integer) : String;
+    function &END : String;
 
   public
     constructor Create(rawStringList: TStringList);
@@ -49,7 +63,7 @@ type
     function GetRawStringData: TStringList;
     procedure DeleteComments(var s: string; CommentSymbol: string);
     function GetErrorMessage: string;
-    function GetExecuteLog: TStringList;
+    function GetExecuteLog: commandLogSum;
     procedure SetErrorMessage(NewValue: string);
     procedure ProcessFile;
     procedure RenderRegisters;
@@ -61,6 +75,7 @@ type
       'CADD', 'CMULT', 'CSUB', 'CDIV', 'JZERO', 'JNZERO');
     RegisterCommands: array[0..5] of string =
       ('LOAD', 'STORE', 'ADD', 'SUB', 'MULT', 'DIV');
+    moveCommands : array[0..3] of String = ('GOTO', 'JZERO', 'JNZERO', 'END');
 
 
   end;
@@ -71,14 +86,125 @@ implementation
 function RegisterMachine.LOAD(c: Integer): String;
 begin
   SetRegisterData(GetRegisterData[c], 0);
-  Result := 'Akkumulator lädt Zahl ' + IntToStr(GetRegisterData[c]) + ' aus c(' + IntToStr(c) + ')';
+  Result := 'A lädt Zahl ' + IntToStr(GetRegisterData[c]) + ' aus c(' + IntToStr(c) + ')';
 end;
 
 function RegisterMachine.STORE(c: Integer): String;
 begin
    SetRegisterData(GetRegisterData[0], c);
-     Result := 'Akkumulator speichert Zahl ' + IntToStr(GetRegisterData[c]) + ' in c(' + IntToStr(c) + ')';
+     Result := 'A speichert Zahl ' + IntToStr(GetRegisterData[c]) + ' in c(' + IntToStr(c) + ')';
 end;
+
+function RegisterMachine.ADD(c: Integer): String;
+begin
+   SetRegisterData(GetRegisterData[0] + GetRegisterData[c], 0);
+   Result := 'A wird um ' + inttoStr(GetRegisterData[c]) + ' aus c(' + IntToStr(c) + ') erhöht';
+end;
+
+function RegisterMachine.SUB(c: Integer): String;
+begin
+   SetRegisterData(GetRegisterData[0] - GetRegisterData[c], 0);
+   if GetRegisterData[0] < 0 then
+   begin
+   GetRegisterData[0] := 0;
+     Result := 'A wird um ' + inttoStr(GetRegisterData[c]) + ' aus c(' + IntToStr(c) + ') bis auf 0 verringert';
+   end
+   else
+     Result := 'A wird um ' + inttostr(GetRegisterData[c]) + ' aus c(' + IntToStr(c) + ') verringert';
+
+end;
+
+function RegisterMachine.MULT(c: Integer): String;
+begin
+   SetRegisterData(GetRegisterData[0] * GetRegisterData[c], 0);
+   Result := 'A wird mit ' + inttostr(GetRegisterData[c]) + ' aus c(' + IntToStr(c) + ') multipliziert';
+end;
+
+function RegisterMachine.&div(c: Integer): String;
+begin
+   if GetRegisterData[c] <> 0 then
+   begin
+   SetRegisterData(GetRegisterData[0] div GetRegisterData[c], 0);
+   Result := 'A wird durch ' + inttostr(GetRegisterData[c]) + ' aus c(' + IntToStr(c) + ') dividiert';
+   end
+   else Result := 'ERROR';
+end;
+
+function RegisterMachine.CLOAD(c: Integer): String;
+begin
+   SetRegisterData(c, 0);
+   Result := 'A lädt Konstante ' + IntToStr(c);
+end;
+
+function RegisterMachine.CADD(c: Integer): String;
+begin
+    SetRegisterData(GetRegisterData[0] + c, 0);
+    Result := 'A um ' + IntToStr(c) + ' erhöht';
+end;
+
+function RegisterMachine.CSUB(c: Integer): String;
+begin
+    SetRegisterData(GetRegisterData[0] - c, 0);
+    Result := 'A um ' + IntToStr(c) + ' verringert';
+end;
+
+function RegisterMachine.CMULT(c: Integer): String;
+begin
+    SetRegisterData(GetRegisterData[0] * c, 0);
+    Result := 'A mit ' + IntToStr(c) + ' multipliziert';
+end;
+
+function RegisterMachine.CDIV(c: Integer): String;
+begin
+   if c <> 0 then
+   begin
+   SetRegisterData(GetRegisterData[0] div c, 0);
+   Result := 'A wird durch ' + IntToStr(c) + ' dividiert';
+   end
+   else Result := 'ERROR';
+end;
+
+function RegisterMachine.JZERO(var j, b: Integer): String;
+begin
+    if GetRegisterData[0] = 0 then
+    begin
+    b := j;
+    Result := 'A ist 0 -> Sprung zur' + IntToStr(j) + '. Programmzeile';
+    end
+    else
+    begin
+    Result := 'A ist nicht 0 -> Sprung zur nächsten Programmzeile';
+    b := b +1;
+    end;
+end;
+
+function RegisterMachine.JNZERO(var j, b: Integer): String;
+begin
+  if GetRegisterData[0] <> 0 then
+    begin
+    b := j;
+    Result := 'A ist nicht 0 -> Sprung zur ' + IntToStr(j) + '. Programmzeile';
+    end
+    else
+    begin
+    Result := 'A ist  0 -> Sprung zur nächsten Programmzeile';
+    b := b +1;
+    end;
+end;
+
+function RegisterMachine.&goto(var j, b: Integer): String;
+begin
+   b := j;
+   Result := 'Sprung zur ' + IntToStr(j) + '. Programmzeile' ;
+end;
+
+function RegisterMachine.&end: String;
+begin
+    Result := 'Ende des Ablaufs';
+end;
+
+
+//FORMATTING AND INITIALIZE
 
 constructor RegisterMachine.Create(rawStringList: TStringList);
 begin
@@ -135,9 +261,9 @@ begin
   Result := errorMessage;
 end;
 
-function RegisterMachine.GetExecuteLog: TStringList;
+function RegisterMachine.GetExecuteLog: commandLogSum;
 begin
-  Result := ExecuteLog;
+  Result := executeLog;
 end;
 
 procedure RegisterMachine.SetErrorMessage(NewValue: string);
@@ -206,13 +332,18 @@ begin
 
     try
       if UpperCase(actualLine.command) <> 'END' then
+      begin
         actualLine.Value :=
-          StrToInt(ExtractDelimited(3, GetRawStringData.Strings[i], [' ']))
+          StrToInt(ExtractDelimited(3, GetRawStringData.Strings[i], [' ']));
+          if actualLine.Value < 0 then
+          SetErrorMessage('illegal value on index ' + IntToStr(index));
+      end
       else
         actualLine.Value := -1;
     except
       SetErrorMessage('illegal value on index ' + IntToStr(index));
     end;
+
     SetProgramData(actualLine, index);
   end;
 
@@ -228,23 +359,56 @@ end;
 procedure RegisterMachine.Execute(regs: registerArray);
 var
   line : commandLine;
-  index, i : Integer;
+  b, i : Integer;
 begin
+  SetLength(executeLog, 1);
   SetRegisterData(0, 0);
   for i := 0 to High(regs) do
   begin
   SetRegisterData(regs[i], i + 1);
   end;
-  index := 0;
+
+  i:= 1;
+  b := 0;
   line.command:= ' ';
   line.Value:= 0;
-  line := GetProgramData[0];
-  ShowMessage(LOAD(2));
-    //while line.command <> 'END' do
-    //begin
-    //
-    //
-    //end;
+  with GetExecuteLog[0] do
+  begin
+  sysOutput:= 'Anfangszustand';
+  registers := Copy(GetRegisterData, 0, Length(GetRegisterData));
+  command.command := '';
+  command.value := 0;
+  b := '';
+  end;
+    while line.command <> 'END' do
+    begin
+      line := GetProgramData[b];
+         SetLength(executeLog, i + 1);
+         GetExecuteLog[i].b := IntToStr(b);
+         case line.command of
+         'LOAD' : GetExecuteLog[i].sysOutput:= LOAD(line.value);
+         'STORE' : GetExecuteLog[i].sysOutput:= STORE(line.value);
+         'ADD' : GetExecuteLog[i].sysOutput:= ADD(line.value);
+         'SUB' : GetExecuteLog[i].sysOutput:= SUB(line.value);
+         'MULT' : GetExecuteLog[i].sysOutput:= MULT(line.value);
+         'DIV' : GetExecuteLog[i].sysOutput:= &DIV(line.value);
+         'CLOAD' : GetExecuteLog[i].sysOutput:= CLOAD(line.value);
+         'CADD' : GetExecuteLog[i].sysOutput:= CADD(line.value);
+         'CSUB' : GetExecuteLog[i].sysOutput:= CSUB(line.value);
+         'CMULT' : GetExecuteLog[i].sysOutput:= CMULT(line.value);
+         'CDIV' : GetExecuteLog[i].sysOutput:= CDIV(line.value);
+         'END' : GetExecuteLog[i].sysOutput:= &END;
+         'GOTO' : GetExecuteLog[i].sysOutput:= &GOTO(line.value, b);
+         'JZERO' : GetExecuteLog[i].sysOutput:= JZERO(line.value, b);
+         'JNZERO' : GetExecuteLog[i].sysOutput:= JNZERO(line.value, b);
+         end;
+
+         GetExecuteLog[i].command := line;
+         GetExecuteLog[i].registers := Copy(GetRegisterData, 0, Length(GetRegisterData));
+         if not AnsiMatchText(line.command, moveCommands) then
+         b := b + 1;
+         i := i + 1;
+    end;
 end;
 
 end.
