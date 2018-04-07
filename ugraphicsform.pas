ecf3aae028729b80a8f724545b63e97880ce1685
DrDegu <JoBa2902@googlemail.com>
2018-04-07 20:55:47 +0200

    Works now, how to is to do

--- ugraphicsform.pas
+++ ugraphicsform.pas
@@ -5,19 +5,29 @@ unit ugraphicsform;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ComCtrls,
-  StdCtrls, ExtCtrls, Grids, windows, strutils, uregistermachine;
+  Classes, SysUtils, FileUtil,  Forms, Controls, Graphics, Dialogs,
+  ComCtrls, StdCtrls, ExtCtrls, Grids, Buttons, ValEdit, Menus, Windows,
+  strutils, uregistermachine;
 
 type
 
-  { TForm1 }
+  { TReMEdit }
 
-  TForm1 = class(TForm)
-    ExecuteBtn: TButton;
-    CreateBtnWrite: TButton;
-    CloseBtn: TButton;
+  TReMEdit = class(TForm)
+    CloseBtn: TBitBtn;
     Editor: TMemo;
+    fillIndicesPop: TPopupMenu;
+    fillIndices: TMenuItem;
+    ErrorOutput: TMemo;
+    HeadingError: TPanel;
+    SpeedContainer: TPanel;
+    SaveRegister: TSaveDialog;
+    SaveFile: TButton;
+    CancelExecuteBtn: TBitBtn;
+    CreateBtnWrite: TButton;
+    ExecuteBtn: TBitBtn;
     InfoLabel: TLabel;
+    SpeedLabel: TLabel;
     LoadFile: TButton;
     OpenRegister: TOpenDialog;
     Pages: TPageControl;
@@ -25,14 +35,22 @@ type
     RegisterSG: TStringGrid;
     ExecuteSG: TStringGrid;
     TabSetUp: TTabSheet;
+    TabError: TTabSheet;
     TabWrite: TTabSheet;
-    TabExecute: TTabSheet;
+    SpeedTrackBar: TTrackBar;
+    procedure CancelExecuteBtnClick(Sender: TObject);
+    procedure fillIndicesClick(Sender: TObject);
+    procedure SaveFileClick(Sender: TObject);
     procedure CloseBtnClick(Sender: TObject);
     procedure CreateMachine(Sender: TObject);
     procedure ExecuteBtnClick(Sender: TObject);
+    procedure ExecuteSGDrawCell(Sender: TObject; aCol, aRow: integer;
+      aRect: TRect; aState: TGridDrawState);
     procedure FormCreate(Sender: TObject);
     procedure LoadFileClick(Sender: TObject);
-    procedure initializeExecuteSG(registerAmount : Integer);
+    procedure initializeExecuteSG(registerAmount: integer);
+    procedure Delay;
+    procedure goToLine(X, Y: integer);
   private
     { private declarations }
   public
@@ -40,145 +58,304 @@ type
   end;
 
 var
-  Form1: TForm1;
-  loadedList : TStringList;
-  regM : RegisterMachine;
+  ReMEdit: TReMEdit;
+  loadedList: TStringList;
+  regM: RegisterMachine;
+  markedCells: array of array[0..1] of integer;
+  executeFinished: boolean;
+  cancelExecute : boolean;
 
 implementation
 
 {$R *.lfm}
+{DELAY FUNCTION}
 
-{ TForm1 }
-procedure TForm1.LoadFileClick(Sender: TObject);
+{ TReMEdit }
+procedure TReMEdit.LoadFileClick(Sender: TObject);
 begin
+  if (Editor.Lines.Count <> 0) and
+    (MessageDlg('', 'Alle nicht gespeicherten Fortschritte gehen verloren. Fortfahren?',
+    mtConfirmation, mbOKCancel, -1) = mrOk) then
+  begin
     if OpenRegister.Execute then
-       begin
-         Editor.Clear;
-         Editor.Lines.LoadFromFile(OpenRegister.FileName);
-       end
-    else ShowMessage('Kein File ausgewählt');
+    begin
+      Editor.Clear;
+      Editor.Lines.LoadFromFile(OpenRegister.FileName);
+    end
+    else
+      ShowMessage('Kein File ausgewählt');
+  end;
 
 end;
 
-procedure TForm1.initializeExecuteSG(registerAmount: Integer);
+procedure TReMEdit.initializeExecuteSG(registerAmount: integer);
 var
-  i : Integer;
+  i: integer;
 begin
-    with ExecuteSG do
-    begin
-      Cells[0, 0] := 'b';
-      Cells[1, 0] := 'Zeile';
-      Cells[2, 0] := 'Effekt';
-      ColCount:= 3 + registerAmount;
-      RowCount := Length(regM.GetExecuteLog) + 1;
-    end;
-    for i := 0 to registerAmount - 1 do
-    begin
-      ExecuteSG.Cells[i + 3, 0 ]:= 'c(' + IntToStr(i) + ')';
-    end;
+  executeFinished := False;
+  RegisterSG.Enabled := False;
+  ExecuteBtn.Enabled := False;
+  CancelExecuteBtn.Enabled := True;
+  with ExecuteSG do
+  begin
+    Enabled := True;
+    Visible := True;
+    ColCount := 3 + registerAmount;
+    RowCount := 1;
+    Cells[0, 0] := 'b';
+    Cells[1, 0] := 'Zeile';
+    Cells[2, 0] := 'Effekt';
+  end;
+  for i := 0 to registerAmount - 1 do
+  begin
+    ExecuteSG.Cells[i + 3, 0] := 'c(' + IntToStr(i) + ')';
+  end;
 end;
 
-procedure TForm1.CloseBtnClick(Sender: TObject);
+procedure TReMEdit.Delay;
+var
+  tc: DWORD;
+begin
+  tc := GetTickCount;
+  while (GetTickCount < tc + ((SpeedTrackBar.Max + SpeedTrackBar.Min) -
+      SpeedTrackBar.Position)) and (not Application.Terminated) do
+    Application.ProcessMessages;
+end;
+
+procedure TReMEdit.goToLine(X, Y: integer);
+var
+  s: string;
+  actLineNumber, i, cPos: integer;
+begin
+  actLineNumber := 0;
+  s := Editor.Text;
+  cPos := 0;
+  for i := 0 to Length(s) do
+  begin
+    cPos := cPos + 1;
+    if Copy(s, i, 1) = #10 then
+      actLineNumber := actLineNumber + 1;
+    if actLineNumber = Y then
+    begin
+      Editor.SetFocus;
+      Editor.SelStart := cPos + X;
+      Break;
+    end;
+  end;
+
+end;
+
+
+procedure TReMEdit.CloseBtnClick(Sender: TObject);
 begin
   Close;
 end;
 
+procedure TReMEdit.SaveFileClick(Sender: TObject);
+begin
+  if SaveRegister.Execute then
+  begin
+    Editor.Lines.SaveToFile(SaveRegister.FileName);
+  end;
+end;
+
+procedure TReMEdit.fillIndicesClick(Sender: TObject);
+var
+  i, b, oldStringLength: integer;
+  pos: TPOINT;
+  line: string;
+  setB: boolean;
+begin
+  pos := editor.CaretPos;
+  b := 0;
+  for i := 0 to Editor.Lines.Count - 1 do
+  begin
+    setB := False;
+    line := trim(Editor.Lines[i]);
+    oldStringLength := Length(line);
+    if RegisterMachine.verifyCommand(line) = '' then
+      setB := True
+    else
+    begin
+      line := '0 ' + line;
+      if RegisterMachine.verifyCommand(line) = '' then
+        setB := True;
+    end;
+    line := ExtractDelimited(2, line, [' ']) + ' ' + ExtractDelimited(3, line, [' ']);
+    if setB = True then
+    begin
+      line := IntToStr(b) + ' ' + line;
+      b := b + 1;
+      Editor.Lines[i] := UpperCase(line);
+      if i = pos.y then
+        pos.x := pos.x + Length(line) - oldStringLength;
+    end;
+  end;
+  goToLine(pos.x - 1, pos.y);
+end;
+
+procedure TReMEdit.CancelExecuteBtnClick(Sender: TObject);
+begin
+   cancelExecute:= true;
+   CancelExecuteBtn.Enabled:= false;
+   ExecuteBtn.Enabled:= true;
+end;
 
 //CREATE
-procedure TForm1.CreateMachine(Sender: TObject);
+procedure TReMEdit.CreateMachine(Sender: TObject);
 var
-  i : Integer;
+  i: integer;
 begin
+
   loadedList := TStringList.Create;
   loadedList.AddStrings(Editor.Lines);
   regM := RegisterMachine.Create(loadedList);
   //ERROR HANDLING NECESSARY
   if regM.GetErrorMessage <> '' then
-     ShowMessage(regM.GetErrorMessage)
+  begin
+    TabError.TabVisible := True;
+    Pages.ActivePage := TabError;
+    ErrorOutput.Clear;
+    ErrorOutput.Lines.Add(regM.GetErrorMessage);
+  end
   else
   begin
-    with  RegisterSG do
+    TabError.TabVisible:=false;
+    with RegisterSG do
     begin
-      ColCount:= High(regM.GetRegisterData);
-      RowCount:= 2;
-      FixedRows:= 1;
-      //Height := RowCount * DefaultRowHeight;
-      //Width := ColCount * DefaultColWidth;
+      RowCount := 2;
+      FixedRows := 1;
+      ColCount := High(regM.GetRegisterData);
     end;
     for i := 0 to High(regM.GetRegisterData) - 1 do
     begin
-        RegisterSG.Cells[i, 0] := 'c(' + IntToStr(i + 1) + ')';
-        RegisterSg.Cells[i, 1] := '0';
+      RegisterSG.Cells[i, 0] := 'c(' + IntToStr(i + 1) + ')';
+      RegisterSG.Cells[i, 1] := '0';
     end;
+    ExecuteSG.Visible := False;
+    TabSetUp.TabVisible := True;
+    Pages.ActivePage := TabSetUp;
+
   end;
-
-  TabSetUp.TabVisible:= true;
-  Pages.ActivePage := TabSetUp;
-
 end;
 
-
 //EXECUTE
-procedure TForm1.ExecuteBtnClick(Sender: TObject);
+procedure TReMEdit.ExecuteBtnClick(Sender: TObject);
 var
-  temp, values: registerArray;
-  i,j : Integer;
-  h : commandLine;
+  values: registerArray;
+  i, j, colorRegister: integer;
+  h: commandLine;
 
 begin
-  SetLength(values, RegisterSG.ColCount - 1 );
-  SetLength(temp, 0);
+  cancelExecute:= false;
+  SetLength(values, RegisterSG.ColCount);
   for i := 0 to RegisterSG.ColCount - 1 do
   begin
-    values[i] := StrtoInt(RegisterSG.Cells[i, 1]);
+    values[i] := StrToInt(RegisterSG.Cells[i, 1]);
   end;
-  regM.Execute(values);
-  TabExecute.TabVisible:= true;
-  Pages.ActivePage := TabExecute;
+  if not regM.Execute(values) then
+  begin
+    if MessageDlg('Warnung','Möglicherweise wird die Registermaschine nie beendet. Ein manueller Abbruch ist bei Fortfahren eventuell erforderlich.', mtWarning, mbOKCancel, 0) = mrCancel then
+    exit;
+  end;
+
 
   initializeExecuteSG(Length(regM.GetRegisterData));
   for i := 1 to Length(regM.GetExecuteLog) do
   begin
+    if cancelExecute then
+    exit;
     with ExecuteSG do
     begin
+      RowCount := RowCount + 1;
+      Row := RowCount;
       Cells[0, i] := regM.GetExecuteLog[i - 1].b;
       h := regM.GetExecuteLog[i - 1].command;
       case h.command of
-           'END' : Cells[1, i] := h.command;
-           '' :   Cells[1, i] := '';
-           else Cells[1, i] := h.command + ' ' + IntToStr(h.value);
+        'END': Cells[1, i] := h.command;
+        '': Cells[1, i] := '';
+        else
+          Cells[1, i] := h.command + ' ' + IntToStr(h.Value);
       end;
       Cells[2, i] := regM.GetExecuteLog[i - 1].sysOutput;
     end;
     for j := 0 to High(regM.GetExecuteLog[i - 1].registers) do
     begin
-    //ExecuteSG.Canvas.Brush.Color:= clwhite;
-
-    ExecuteSG.Cells[3 + j, i] := IntToStr(regM.GetExecuteLog[i -1].registers[j]);
-    if i <> 1 then
-    begin
-       if regM.GetExecuteLog[i - 1].registers[j] <> temp[j] then
-          ExecuteSG.Cells[3 + j, i] := ExecuteSG.Cells[3 + j, i] + '*';
-       end;
+      ExecuteSG.Cells[3 + j, i] := IntToStr(regM.GetExecuteLog[i - 1].registers[j]);
     end;
-    temp := Copy(regM.GetExecuteLog[i -1].registers, 0, Length(regM.GetExecuteLog[i -1].registers));
-  end;
 
+    colorRegister := -1;
+    if AnsiMatchText(regM.GetExecuteLog[i - 1].command.command,
+      ['LOAD', 'CLOAD', 'CADD', 'CSUB', 'CMULT', 'CDIV', 'MULT', 'ADD', 'SUB', 'DIV']) then
+      colorRegister := 0
+    else if regM.GetExecuteLog[i - 1].command.command = 'STORE' then
+      colorRegister := regM.GetExecuteLog[i - 1].command.Value;
+
+    if colorRegister <> -1 then
+    begin
+      SetLength(markedCells, Length(markedCells) + 1);
+      markedCells[High(markedCells)][0] := 3 + colorRegister;
+      markedCells[High(markedCells)][1] := i;
+    end;
+
+    if i = Length(regM.GetExecuteLog) then
+      executeFinished := True;
+
+    Delay;
+  end;
+  CancelExecuteBtnClick(nil);
 
 end;
 
-procedure TForm1.FormCreate(Sender: TObject);
+procedure TReMEdit.ExecuteSGDrawCell(Sender: TObject; aCol, aRow: integer;
+  aRect: TRect; aState: TGridDrawState);
 var
-  s : TTextStyle;
+  i: integer;
+
+  procedure colorCell(aRect: TRect; cellText: String; brushColor: TColor);
+  begin
+    with ExecuteSG do
+    begin
+    Canvas.Brush.Color := brushColor;
+    Canvas.FillRect(aRect);
+    Canvas.TextOut(aRect.Left + 2, aRect.Top + 2, cellText);
+    end;
+  end;
+
 begin
+  for i := 0 to High(markedCells) do
+  begin
+    if (ACol = markedCells[i][0]) and (ARow = markedCells[i][1]) then
+    begin
+       colorCell(aRect, ExecuteSG.Cells[ACol, ARow], clSilver);
+    end;
+  end;
+
+  if ARow = Length(regM.GetExecuteLog) then
+  begin
+    colorCell(aRect, ExecuteSG.Cells[ACol, ARow], clGreen);
+  end;
+
+end;
+
+procedure TReMEdit.FormCreate(Sender: TObject);
+var
+  s: TTextStyle;
+begin
+  //VARIABLE INITIALIZATION
+  cancelExecute:= false;
+  SetLength(markedCells, 0);
   Editor.Lines.LoadFromFile('Beispiele/Zahlenvergleich.txt');
+
+  //LAYOUT INITIALIZATION
   Pages.ActivePage := TabWrite;
-  TabSetUp.TabVisible:= false;
-  TabExecute.TabVisible:= false;
-  s := RegisterSG.DefaultTextStyle;
-  s.Alignment:=taCenter;
-  RegisterSG.DefaultTextStyle := s;
+  TabSetUp.TabVisible := False;
+  TabError.TabVisible:= false;
+  Pages.Color := clGray;
+
+  //
+
+
 end;
 
 end.
-
--- uregistermachine.pas
+++ uregistermachine.pas
@@ -10,11 +10,11 @@ uses
 type
   commandLine = record
     command: string;
-    Value: integer;
+    Value: Word;
   end;
 
 type
-  registerArray = array of integer;
+  registerArray = array of Longword;
   commandList = array of commandLine;
 
   commandLog = record
@@ -37,38 +37,39 @@ type
     rawStringData: TStringList;
     executeLog: commandLogSum;
     errorMessage: string;
-    function LOAD(c : Integer) : String;
-    function STORE(c : Integer) : String;
-    function ADD(c : Integer) : String;
-    function SUB(c : Integer) : String;
-    function MULT(c : Integer) : String;
-    function &DIV(c : Integer) : String;
-    function CLOAD(c : Integer) : String;
-    function CADD(c : Integer) : String;
-    function CSUB(c : Integer) : String;
-    function CMULT(c : Integer) : String;
-    function CDIV(c : Integer) : String;
-    function JZERO(var j, b : Integer) : String;
-    function JNZERO(var j, b : Integer) : String;
-    function &GOTO(var j, b : Integer) : String;
+    function LOAD(c: Word): String;
+    function STORE(c : Word) : String;
+    function ADD(c : Word) : String;
+    function SUB(c : Word) : String;
+    function MULT(c : Word) : String;
+    function &DIV(c : Word) : String;
+    function CLOAD(c: Word): String;
+    function CADD(c : Longword) : String;
+    function CSUB(c : Longword) : String;
+    function CMULT(c : Longword) : String;
+    function CDIV(c : Longword) : String;
+    function JZERO(var j, b : Word) : String;
+    function JNZERO(var j, b : Word) : String;
+    function &GOTO(var j, b : Word) : String;
     function &END : String;
 
   public
     constructor Create(rawStringList: TStringList);
+    class function verifyCommand(s: String): String;
     function GetRegisterData: registerArray;
-    procedure SetRegisterData(NewValue: integer; Index: integer);
+    procedure SetRegisterData(NewValue: Longword; Index: integer);
     procedure SetRegisterData(NewArray: registerArray);
     function GetProgramData: commandList;
     procedure SetProgramData(NewValue: commandLine; Index: integer);
     function GetRawStringData: TStringList;
-    procedure DeleteComments(var s: string; CommentSymbol: string);
+    class procedure DeleteComments(var s: string; CommentSymbol: string);
     function GetErrorMessage: string;
     function GetExecuteLog: commandLogSum;
     procedure SetErrorMessage(NewValue: string);
     procedure ProcessFile;
     procedure RenderRegisters;
     procedure RenderCommands;
-    procedure Execute(regs : registerArray);
+    function Execute(regs : registerArray): boolean;
   const
     commands: array[0..14] of string =
       ('LOAD', 'STORE', 'ADD', 'SUB', 'MULT', 'DIV', 'GOTO', 'END', 'CLOAD',
@@ -83,44 +84,49 @@ type
 implementation
 
 //REGISTER MACHINE FUNCTIONS
-function RegisterMachine.LOAD(c: Integer): String;
+function RegisterMachine.LOAD(c: Word): String;
 begin
   SetRegisterData(GetRegisterData[c], 0);
   Result := 'A lädt Zahl ' + IntToStr(GetRegisterData[c]) + ' aus c(' + IntToStr(c) + ')';
 end;
 
-function RegisterMachine.STORE(c: Integer): String;
+function RegisterMachine.STORE(c: Word): String;
 begin
    SetRegisterData(GetRegisterData[0], c);
      Result := 'A speichert Zahl ' + IntToStr(GetRegisterData[c]) + ' in c(' + IntToStr(c) + ')';
 end;
 
-function RegisterMachine.ADD(c: Integer): String;
+function RegisterMachine.ADD(c: Word): String;
 begin
    SetRegisterData(GetRegisterData[0] + GetRegisterData[c], 0);
    Result := 'A wird um ' + inttoStr(GetRegisterData[c]) + ' aus c(' + IntToStr(c) + ') erhöht';
 end;
 
-function RegisterMachine.SUB(c: Integer): String;
+function RegisterMachine.SUB(c: Word): String;
+var
+  res : Integer;
 begin
-   SetRegisterData(GetRegisterData[0] - GetRegisterData[c], 0);
-   if GetRegisterData[0] < 0 then
+   res := GetRegisterData[0] - GetRegisterData[c];
+   if res < 0 then
    begin
    GetRegisterData[0] := 0;
      Result := 'A wird um ' + inttoStr(GetRegisterData[c]) + ' aus c(' + IntToStr(c) + ') bis auf 0 verringert';
    end
    else
+   begin
+     SetRegisterData(res, 0);
      Result := 'A wird um ' + inttostr(GetRegisterData[c]) + ' aus c(' + IntToStr(c) + ') verringert';
+   end;
 
 end;
 
-function RegisterMachine.MULT(c: Integer): String;
+function RegisterMachine.MULT(c: Word): String;
 begin
    SetRegisterData(GetRegisterData[0] * GetRegisterData[c], 0);
    Result := 'A wird mit ' + inttostr(GetRegisterData[c]) + ' aus c(' + IntToStr(c) + ') multipliziert';
 end;
 
-function RegisterMachine.&div(c: Integer): String;
+function RegisterMachine.&div(c: Word): String;
 begin
    if GetRegisterData[c] <> 0 then
    begin
@@ -130,31 +136,31 @@ begin
    else Result := 'ERROR';
 end;
 
-function RegisterMachine.CLOAD(c: Integer): String;
+function RegisterMachine.CLOAD(c: Word): String;
 begin
    SetRegisterData(c, 0);
    Result := 'A lädt Konstante ' + IntToStr(c);
 end;
 
-function RegisterMachine.CADD(c: Integer): String;
+function RegisterMachine.CADD(c: Longword): String;
 begin
     SetRegisterData(GetRegisterData[0] + c, 0);
     Result := 'A um ' + IntToStr(c) + ' erhöht';
 end;
 
-function RegisterMachine.CSUB(c: Integer): String;
+function RegisterMachine.CSUB(c: Longword): String;
 begin
     SetRegisterData(GetRegisterData[0] - c, 0);
     Result := 'A um ' + IntToStr(c) + ' verringert';
 end;
 
-function RegisterMachine.CMULT(c: Integer): String;
+function RegisterMachine.CMULT(c: Longword): String;
 begin
     SetRegisterData(GetRegisterData[0] * c, 0);
     Result := 'A mit ' + IntToStr(c) + ' multipliziert';
 end;
 
-function RegisterMachine.CDIV(c: Integer): String;
+function RegisterMachine.CDIV(c: Longword): String;
 begin
    if c <> 0 then
    begin
@@ -164,12 +170,12 @@ begin
    else Result := 'ERROR';
 end;
 
-function RegisterMachine.JZERO(var j, b: Integer): String;
+function RegisterMachine.JZERO(var j, b: Word): String;
 begin
     if GetRegisterData[0] = 0 then
     begin
     b := j;
-    Result := 'A ist 0 -> Sprung zur' + IntToStr(j) + '. Programmzeile';
+    Result := 'A ist 0 -> Sprung zur' + IntToStr(j) + ' . Programmzeile';
     end
     else
     begin
@@ -178,7 +184,7 @@ begin
     end;
 end;
 
-function RegisterMachine.JNZERO(var j, b: Integer): String;
+function RegisterMachine.JNZERO(var j, b: Word): String;
 begin
   if GetRegisterData[0] <> 0 then
     begin
@@ -192,7 +198,7 @@ begin
     end;
 end;
 
-function RegisterMachine.&goto(var j, b: Integer): String;
+function RegisterMachine.&goto(var j, b: Word): String;
 begin
    b := j;
    Result := 'Sprung zur ' + IntToStr(j) + '. Programmzeile' ;
@@ -215,12 +221,48 @@ begin
   RenderRegisters;
 end;
 
+{checks if command is legal
+0: legal
+ERRORS:
+1: illegal index
+2: illegal command
+3: illegal value
+}
+class function RegisterMachine.verifyCommand(s: String): String;
+var
+  temp : Integer;
+  line, errM: String;
+
+  procedure addToResult(var s: String; const addS: String);
+  begin
+    if s <> '' then
+      s := s +  sLineBreak + addS
+    else
+      s := addS;
+  end;
+
+begin
+  errM := '';
+  DeleteComments(s, '//');
+  s:= trim(s);
+  line := UpperCase(ExtractDelimited(2, s, [' ']));
+  if not AnsiMatchText(line, commands) then
+    addToResult(errM, s + ': illegal command ' + line);
+  if not TryStrToInt(ExtractDelimited(1, s, [' ']), temp) then
+    addToResult(errM, s + ': illegal index ' + ExtractDelimited(1, s, [' ']));
+  if (line <> 'END') AND (not TryStrToInt(ExtractDelimited(3, s, [' ']), temp)) then
+    addToResult(errM, s + ': illegal value ' + ExtractDelimited(3, s, [' ']));
+  if (line = 'END') AND (Pos('END',s) <> Length(s) - 2) then
+    addToResult(errM, s + ': illegal value ' + ExtractDelimited(3, s, [' ']));
+  Result := errM;
+end;
+
 function RegisterMachine.GetRegisterData: registerArray;
 begin
   Result := registerData;
 end;
 
-procedure RegisterMachine.SetRegisterData(NewValue: integer; Index: integer);
+procedure RegisterMachine.SetRegisterData(NewValue: Longword; Index: integer);
 begin
   registerData[Index] := NewValue;
 end;
@@ -247,7 +289,8 @@ begin
   Result := rawStringData;
 end;
 
-procedure RegisterMachine.DeleteComments(var s: string; CommentSymbol: string);
+class procedure RegisterMachine.DeleteComments(var s: string;
+  CommentSymbol: string);
 var
   position: integer;
 begin
@@ -313,54 +356,41 @@ end;
 
 procedure RegisterMachine.RenderCommands;
 var
-  i, index: integer;
+  i: integer;
   actualLine: commandLine;
+  error : String;
 begin
   for i := 0 to GetRawStringData.Count - 1 do
   begin
-    try
-      index := StrToInt(ExtractDelimited(1, GetRawStringData.Strings[i], [' ']));
-    except
-      SetErrorMessage('illegal index ' + IntToStr(index));
-    end;
-
-    actualLine.command := ExtractDelimited(2, GetRawStringData.Strings[i], [' ']);
-    if not AnsiMatchText(actualLine.command, commands) then
-    begin
-      SetErrorMessage('illegal command on index ' + IntToStr(index));
-    end;
-
-    try
-      if UpperCase(actualLine.command) <> 'END' then
-      begin
+       error:= verifyCommand(GetRawStringData.Strings[i]);
+     if error = '' then
+     begin
+        actualLine.command := UpperCase(ExtractDelimited(2, GetRawStringData.Strings[i], [' ']));
+        if actualLine.command <> 'END' then
         actualLine.Value :=
-          StrToInt(ExtractDelimited(3, GetRawStringData.Strings[i], [' ']));
-          if actualLine.Value < 0 then
-          SetErrorMessage('illegal value on index ' + IntToStr(index));
-      end
-      else
-        actualLine.Value := -1;
-    except
-      SetErrorMessage('illegal value on index ' + IntToStr(index));
-    end;
-
-    SetProgramData(actualLine, index);
+          StrToInt(ExtractDelimited(3, GetRawStringData.Strings[i], [' ']))
+        else
+        actualLine.Value:= 0;
+        SetProgramData(actualLine, StrtoInt(ExtractDelimited(1, GetRawStringData.Strings[i], [' '])));
+     end
+     else
+     SetErrorMessage(error);
   end;
-
-  for i := 0 to Length(GetProgramData) do
+  for i := 0 to High(GetProgramData) do
   begin
-    if GetProgramData[i].Value = 0 then
-    begin
-      SetErrorMessage('index ' + IntToStr(i) + ' is not initialized');
-    end;
+     if GetProgramData[i].command = 'END' then
+     exit;
   end;
+  SetErrorMessage('no END command found');
 end;
 
-procedure RegisterMachine.Execute(regs: registerArray);
+function RegisterMachine.Execute(regs: registerArray): boolean;
 var
   line : commandLine;
-  b, i : Integer;
+  b : Word;
+  i : Integer;
 begin
+  Result := true;
   SetLength(executeLog, 1);
   SetRegisterData(0, 0);
   for i := 0 to High(regs) do
@@ -408,6 +438,12 @@ begin
          if not AnsiMatchText(line.command, moveCommands) then
          b := b + 1;
          i := i + 1;
+
+         if i > 1000 then
+         begin
+         Result := false;
+         Break;
+         end;
     end;
 end;
 
